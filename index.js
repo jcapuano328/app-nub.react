570dffea5f20255981801dd7a73a34c06e843a69
Jeff Capuano <jcapuano328@gmail.com>
2016-10-06 06:45:33 -0400

    exports

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 module.exports = {
+    // services
     Log: require('./src/services/log'),
     Range: require('./src/services/range'),
     Store: require('./src/services/store'),
+    // widgets
+    Arrow: require('./src/widgets/arrow'),
+    Checkbox: require('./src/widgets/checkbox'),
+    DatetimePicker: require('./src/widgets/datetimePicker.android'),
+    DrawerLayout: require('./src/widgets/drawerLayout.android'),
+    IconButton: require('./src/widgets/iconButton'),
+    MultiSelectList: require('./src/widgets/multiSelectList'),
+    NavMenu: require('./src/widgets/navMenu'),
+    NavMenuItem: require('./src/widgets/navMenuItem'),
+    RadioButton: require('./src/widgets/radioButton'),
+    SelectDropdown: require('./src/widgets/selectDropdown'),
+    SelectList: require('./src/widgets/selectList'),
+    SpinButton: require('./src/widgets/spinButton'),
+    SpinNumeric: require('./src/widgets/spinNumeric'),
+    SpinSelect: require('./src/widgets/spinSelect'),
+    TitleBar: require('./src/widgets/titleBar'),
+    // views
+    AboutView: require('./src/views/aboutView'),
+    LandingView: require('./src/views/landingView')
 };
