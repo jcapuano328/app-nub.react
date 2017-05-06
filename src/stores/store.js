b2387c8fa10366b8134f20b1cf6d4b8610c81e3b
Jeff Capuano <jcapuano328@gmail.com>
2017-05-06 08:06:18 -0400

    control redux logging

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,14 +3,14 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 
-const middlewares = [thunk];
-if (process.env.NODE_ENV !== 'production') {
-    const createLogger = require('redux-logger');
-    const logger = createLogger();
-    middlewares.push(logger);
-}
-
-module.exports = (reducer, done) => {
+module.exports = (reducer, done, nologging) => {
+    const middlewares = [thunk];
+    if (!nologging || process.env.NODE_ENV !== 'production') {
+        const createLogger = require('redux-logger');
+        const logger = createLogger();
+        middlewares.push(logger);
+    }
+    
     const store = compose(applyMiddleware(...middlewares))(createStore)(reducer);
 
     persistStore(store, { storage: AsyncStorage }, done);
